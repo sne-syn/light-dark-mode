037e07262a6b6aae470e45c6bca5b9af8a1dd517
sne_syn <sss@anadeainc.com>
2021-07-24 15:11:48 +0200

    Final adjustments

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
-const toggleSwitch = document.querySelector(‘input[type=“checkbox”]’);
-const nav = document.getElementById(‘nav’);
-const toggleIcon = document.getElementById(‘toggle-icon’);
-const image1 = document.getElementById(‘image1’);
-const image2 = document.getElementById(‘image2’);
-const image3 = document.getElementById(‘iamge3’);
-const textBox = document.getElementById(‘text-box’);
-const darkMode = () => {
-  nav.style.backgroundColor = ‘rgb(0 0 0 / 50%)’;
-  textBox.style.backgroundColor = ‘rgb(255 255 255 /50%)’;
-  toggleIcon.children[0].textContent = ‘Dark Mode’;
-  toggleIcon.children[1].classList.replace(‘fa-sun’, ‘fa-moon’);
-  image1.src = ‘img/undraw_proud_coder_dark.svg’;
-  image2.src = ‘img/undraw_feeling_proud_dark.svg’;
-  image3.src = ‘img/undraw_conceptual_idea_dark.svg’;
+const Icons = {
+  DARK: 'fa-moon',
+  LIGHT: 'fa-sun'
 }
-const lightMode = () => {
-  nav.style.backgroundColor = ‘rgb(255 255 255 /50%)’;
-  textBox.style.backgroundColor = ‘rgb(0 0 0 / 50%)’;
-  toggleIcon.children[0].textContent = ‘Light Mode’;
-  toggleIcon.children[1].classList.replace(‘fa-moon’, ‘fa-sun’);
-  image1.src = ‘img/undraw_proud_coder_light.svg’;
-  image2.src = ‘img/undraw_feeling_proud_light.svg’;
-  image3.src = ‘img/undraw_conceptual_idea_light.svg’;
+const modeNames = {
+  DARK: 'dark',
+  LIGHT: 'light'
 }
-const switchTheme = (e) => {
-  if (e.target.checked) {
-    document.documentElement.setAttribute(‘data-theme’, ‘dark’);
-    darkMode();
+
+const images = document.querySelectorAll('.image');
+const nav = document.getElementById('nav');
+const toggleElement = document.getElementById('toggle-element');
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const textBox = document.getElementById('text-box');
+
+const capitalizeString = string => string.charAt(0).toUpperCase() + string.slice(1);
+
+const changeElementsMode = (modeType) => {
+  const isLight = modeType === modeNames.LIGHT;
+  const text = capitalizeString(modeType);
+  nav.style.backgroundColor = (isLight) ? 'rgb(255 255 255 /50%)' : 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = (isLight) ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 /50%)';
+  toggleElement.children[0].textContent = `${text} Mode`;
+  images.forEach(img => img.src = (img.src.match('light.svg')) ? img.src.replace('light.svg', 'dark.svg') : img.src.replace('dark.svg', 'light.svg'));
+  if (isLight) {
+    toggleElement.children[1].classList.replace(Icons.DARK, Icons.LIGHT);
   } else {
-    document.documentElement.setAttribute(‘data-theme’, ‘light’);
-    lightMode();
+    toggleElement.children[1].classList.replace(Icons.LIGHT, Icons.DARK);
   }
 }
-toggleSwitch.addEventListener(‘change’, switchTheme);
+
+const switchTheme = (e) => {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', modeNames.DARK);
+    localStorage.setItem('theme', modeNames.DARK);
+    changeElementsMode(modeNames.DARK);
+  } else {
+    document.documentElement.setAttribute('data-theme', modeNames.LIGHT);
+    localStorage.setItem('theme', modeNames.LIGHT);
+    changeElementsMode(modeNames.LIGHT);
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    changeElementsMode(currentTheme)
+  }
+}
